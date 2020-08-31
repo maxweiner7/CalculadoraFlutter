94573d88bb0bf7a9ef4793253df8dc153f395ed6
maxweiner7 <max.wener@hotmail.com>
2020-08-31 12:13:07 +0100

    Calculadora Mac

---
+++ android/app/src/main/java/com/weiner/calculator/MainActivity.java
@@ -0,0 +1,6 @@
+package com.weiner.calculator;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/calculator.dart
@@ -0,0 +1,39 @@
+import 'package:calculator/models/memory.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../components/display.dart';
+import '../components/keyboard.dart';
+
+
+class Calculator extends StatefulWidget {
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}  final Memory memory = Memory();
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+      setState(() {
+        memory.applyCommand(command);
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Column(
+        children: <Widget>[
+          Display(memory.value),
+          Keyboard(_onPressed),
+        ],
+      ),
+    );
+  }
+}
